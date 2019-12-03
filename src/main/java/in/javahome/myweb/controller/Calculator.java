4d8e22b5627e22418d1d326827683444a29c6783
prveen7 <prveen7@gmail.com>
2019-12-03 12:14:18 +0530

    pom files

---
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -0,0 +1,19 @@
+package in.javahome.myweb.controller;
+/*
+ * 
+ */
+public class Calculator {
+	/*
+	 * @praveen 
+	 * @praveen
+	 * @return int
+	 */
+	public int add(int i, int j){
+		String s = null;
+		s.toString();
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
+}
